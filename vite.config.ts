c90a7673a6be667d0000d59987df06890bae4e08
Haeram Kim <haeram.kim@doosan.com>
2025-09-29 16:59:44 +0900

    fix: router settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./",
+  base: "/glossary-admin/",
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
